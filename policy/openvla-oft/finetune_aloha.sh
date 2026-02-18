8ba93fb2c341cb2ecd40ebd94e735d3c90f194f4
zixiao_bios <zixiao_bios@outlook.com>
2026-02-19 00:28:48 +0800

    修复训练配置

--- policy/openvla-oft/finetune_aloha.sh
+++ policy/openvla-oft/finetune_aloha.sh
@@ -1,18 +1,18 @@
 # In experiments, global batch size of less than 16 will easily lead to unsuccessful training, where the training and validation 
 # loss would not converge low enough, and the final policy would repeat one trajectory regardless of the visual and language inputs.
 export WANDB_MODE=disabled
-torchrun --standalone --nnodes 1 --nproc-per-node 3 vla-scripts/finetune.py \
+torchrun --standalone --nnodes 1 --nproc-per-node 4 vla-scripts/finetune.py \
   --vla_path /root/autodl-tmp/OpenVLA-7B \
   --data_root_dir /root/tensorflow_datasets \
   --dataset_name aloha_beat_block_hammer \
   --run_root_dir /root/autodl-tmp/RoboTwin/ckpt \
   --use_l1_regression True \
   --use_diffusion False \
-  --use_film False \
+  --use_film True \
   --num_images_in_input 3 \
   --use_fsdp True \
   --gradient_checkpointing True \
-  --grad_accumulation_steps 6 \
+  --grad_accumulation_steps 4 \
   --use_proprio True \
   --batch_size 1 \
   --learning_rate 5e-4 \
@@ -20,7 +20,7 @@ torchrun --standalone --nnodes 1 --nproc-per-node 3 vla-scripts/finetune.py \
   --max_steps 100005 \
   --use_val_set True \
   --val_freq 1000 \
-  --save_freq 5000 \
+  --save_freq 1 \
   --save_latest_checkpoint_only False \
   --image_aug True \
   --lora_rank 32 \
